83f548e743b7f038f5d2409812805a7de833a163
hooan <hooan.cristobal@gmail.com>
2022-02-10 20:01:40 -0600

    Update PaymentsService.java

--- src/main/java/com/aplazo/api/services/PaymentsService.java
+++ src/main/java/com/aplazo/api/services/PaymentsService.java
@@ -8,6 +8,8 @@ import com.aplazo.api.repositories.OutputRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,19 +25,19 @@ public class PaymentsService {
 
     public List<OutputResponse> getPayments(Input input){
         Input response = inputRepository.save(input);
-        Double realAmount = input.getAmount() * (1 + (input.getRate() * input.getTerms()));
+        Double realAmount = input.getAmount() * (1 + (input.getRate()/100));
         List<OutputResponse> result = new ArrayList<>();
-        Double pendingAmount = realAmount;
-        Double paymentAmount = realAmount/input.getTerms();
+        BigDecimal pendingAmount = new BigDecimal(realAmount).setScale(2);
+        BigDecimal paymentAmount = new BigDecimal(realAmount/input.getTerms()).setScale(2,RoundingMode.UP);
         LocalDate payDate = LocalDate.now();
         for (int i = 1; i <= input.getTerms(); i++) {
             payDate = payDate.plusWeeks(1);
-            if(pendingAmount - paymentAmount < 0){
+            if(pendingAmount.subtract(paymentAmount).doubleValue() < 0.0){
                 paymentAmount = pendingAmount;
             }
-            pendingAmount = pendingAmount - paymentAmount;
-            result.add(new OutputResponse(i,paymentAmount,pendingAmount,payDate.toString()));
-            outputRepository.save(new Output(i,paymentAmount,pendingAmount,payDate.toString(),response.getId()));
+            pendingAmount = pendingAmount.subtract(paymentAmount);
+            result.add(new OutputResponse(i,paymentAmount.doubleValue(),pendingAmount.doubleValue(),payDate.toString()));
+            outputRepository.save(new Output(i,paymentAmount.doubleValue(),pendingAmount.doubleValue(),payDate.toString(),response.getId()));
         }
         return result;
     }
