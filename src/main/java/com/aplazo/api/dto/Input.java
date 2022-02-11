6a7a58b5c7d2c4afcb60091dfff9c4a452d8c410
Cristobal Lopez <>
2022-02-10 20:52:16 -0600

    Format validation error msgs

--- src/main/java/com/aplazo/api/dto/Input.java
+++ src/main/java/com/aplazo/api/dto/Input.java
@@ -11,16 +11,16 @@ import javax.validation.constraints.*;
 @Data
 public class Input{
 
-    @DecimalMin( value = "1.00",message = "Min $1.00")
-    @DecimalMax( value = "999999.00", message = " Max $999,999.00")
+    @DecimalMin( value = "1.00",message = "Amount min $1.00")
+    @DecimalMax( value = "999999.00", message = "Amount max $999,999.00")
     private Double amount;
 
-    @Min( value= 4, message = "Min 4")
-    @Max( value = 52,message = "Max 52")
+    @Min( value= 4, message = "Terms min 4")
+    @Max( value = 52,message = "Terms max 52")
     private Integer terms;
 
-    @Min( value= 0, message = "Min 0")
-    @Max( value = 100,message = "Max 100")
+    @Min( value= 0, message = "Rate min 0")
+    @Max( value = 100,message = "Rate max 100")
     private Double rate;
 
     @Id
